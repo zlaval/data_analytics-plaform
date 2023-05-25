65e88b90ea0695cad6cd58e896ab3b4cb3c37001
zlaval <zlaval@gmail.com>
2023-05-25 20:10:00 +0200

    fix product dbt

--- transformations/models/base/products.sql
+++ transformations/models/base/products.sql
@@ -12,16 +12,23 @@
 select
 
     ph.product_id as id,
-    COALESCE (max(ph.name),max(t.name) )as name,
+
+    {% if is_incremental() %}
+        COALESCE (max(ph.name),max(t.name) ) as name,
+    {% else %}
+        max(ph.name) as name,
+    {% endif %}    
+   
     (max(array[to_json(ph.modified_at)::text,ph.price::text]))[2]::int as price,
     max(ph.sync_date) as sync_date
 
 
 from
     {{ref('products_history')}} ph
-    left outer join {{this}} t on t.id=ph.product_id
+  
 
 {% if is_incremental() %}
+    left outer join {{this}} t on t.id=ph.product_id
     where ph.id in(
         select id from {{ref('products_history')}}
         group by id
